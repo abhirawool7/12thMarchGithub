31c4e165d003c638e7008f9faadae042d87fa01d
DR <DR@DESKTOP-OIABPBF>
2022-07-20 18:30:11 +0530

    [Abhi]:added new project

---
+++ src/Sample/Demo.java
@@ -0,0 +1,17 @@
+package Sample;
+
+public class Demo
+{
+  public static void main(String[] args)
+  {
+	
+      System.out.println("HI");  
+
+      System.out.println("HI");  
+	  
+      System.out.println("HELLO");  
+	  
+      System.out.println("HELLO");  
+      
+}
+}
